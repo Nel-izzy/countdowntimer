e1dca7861485a429c6c260da31999d32c16f53b1
nelson <blackcodedigitals@gmail.com>
2020-06-17 07:04:17 +0100

    Updates

---
+++ precache-manifest.bb057e470a29341c5b848e64d5d4af31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90cb892978a00d9009aa66fd96bd369f",
+    "url": "/countdowntimer/index.html"
+  },
+  {
+    "revision": "566c4d64e17711121226",
+    "url": "/countdowntimer/static/js/2.82be1f96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countdowntimer/static/js/2.82be1f96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aec92f406e5eef0b98f8",
+    "url": "/countdowntimer/static/js/main.8aa23d3b.chunk.js"
+  },
+  {
+    "revision": "5a351214664ffe43f7d1",
+    "url": "/countdowntimer/static/js/runtime-main.77f7748d.js"
+  }
+]);
